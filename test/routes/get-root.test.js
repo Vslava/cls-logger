e6eec0213a232f421a7cabb1fd0168048c70ad6e
Viacheslav Voytovich <voytovich.slava@gmail.com>
2020-12-21 23:31:11 +0700

    make get-root test

--- test/routes/get-root.test.js
+++ test/routes/get-root.test.js
@@ -10,12 +10,32 @@ describe(__filename, () => {
 
   it('gets the page', async function () {
     // init
-    factory.create('TestData');
+    const record1 = await factory.create('TestData');
+    const record2 = await factory.create('TestData');
 
     // process
     // check
-    this.server
+    await this.server
       .get('/')
-      .expect('');
+      .expect([
+        '<!DOCTYPE html>',
+        '<html>',
+        '  <head>',
+        '    <meta charset="utf-8">',
+        '    <title>Test Devops Page</title>',
+        '  </head>',
+        '  <body>',
+        '    <h3>Test Devops Page</h3>',
+        '      <div>',
+        `        <h4>${record1.title}</h4>`,
+        `        <p>${record1.description}</p>`,
+        '      </div>',
+        '      <div>',
+        `        <h4>${record2.title}</h4>`,
+        `        <p>${record2.description}</p>`,
+        '      </div>',
+        '  </body>',
+        '</html>',
+      ].join('\n'));
   });
 });
